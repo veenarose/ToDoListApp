445faaf53a2af600bfcc2ac70336ace6e71a1a13
veenarose <mathew.cateyes@gmail.com>
2016-02-17 03:14:59 -0500

    Activity 2 UX

--- app/src/main/java/com/veenamathews/todolistapp/MainActivity.java
+++ app/src/main/java/com/veenamathews/todolistapp/MainActivity.java
@@ -1,16 +1,66 @@
 package com.veenamathews.todolistapp;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String KEY_EXTRA_CONTACT_ID = "KEY_EXTRA_CONTACT_ID";
+
+    private ListView listView;
+    DataBase dataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTask = (Button) findViewById(R.id.button);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateOrEditTask.class);
+                intent.putExtra(KEY_EXTRA_CONTACT_ID, 0);
+                startActivity(intent);
+            }
+        });
+
+        dataBase = new DataBase(this);
+
+        final Cursor cursor = dataBase.getAllTasks();
+        String[] columns = new String[]{
+                DataBase.COLUMN_ID,
+                DataBase.COLUMN_TITLE
+        };
+        int[] widgets = new int[]{
+
+        };
+
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(this, R.layout.tasks_layout,
+                cursor, columns, widgets, 0);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(cursorAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView listView, View view,
+                                    int position, long id) {
+                Cursor itemCursor = (Cursor) MainActivity.this.listView.getItemAtPosition(position);
+                int taskID = itemCursor.getInt(itemCursor.getColumnIndex(DataBase.COLUMN_ID));
+                Intent intent = new Intent(getApplicationContext(), CreateOrEditTask.class);
+                intent.putExtra(KEY_EXTRA_CONTACT_ID, taskID);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
